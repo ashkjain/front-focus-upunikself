11054054a8b02b0c89269e8ac742ef3b58695e15
Ashish Jain <ashishjain99@yahoo.com>
2024-08-29 09:29:18 -0600

    Added Thank You Page

---
+++ js/retrieve.js
@@ -0,0 +1,23 @@
+function retrieveFormData()
+{
+    const firstName = localStorage.getItem('fname');
+    const lastName = localStorage.getItem('lname');
+    const email = localStorage.getItem('email');
+
+    const displayName = document.getElementById('fullName');
+    const displayEmail = document.getElementById('email');
+
+    if(lastName == "")
+    {
+        displayName.innerText = `${firstName[0]+firstName.slice(1)}`;
+        displayEmail.innerText = email;
+    }
+    else
+    {
+        displayName.innerText = `${firstName[0].toUpperCase()+firstName.slice(1)} ${lastName[0].toUpperCase()+lastName.slice(1)}`;
+        displayEmail.innerText = email;
+    }
+    
+};
+
+retrieveFormData();
--- js/script.js
+++ js/script.js
@@ -15,5 +15,26 @@ function copyrightYear()
     const yearElement = document.getElementById('year');
     yearElement.innerText = currentYear;   
 };
+
+function getFormData()
+{
+    const contactForm = document.getElementById('form');
+    contactForm.addEventListener('submit', (event) => 
+        {
+            event.preventDefault();
+
+            const firstName = document.getElementById('fname').value;
+            const lastName = document.getElementById('lname').value;
+            const email = document.getElementById('email').value;
+
+            localStorage.setItem('fname', firstName);
+            localStorage.setItem('lname', lastName);
+            localStorage.setItem('email', email);
+
+            window.location.href = 'thanks.html';
+        });
+};
+
 hamburgerToggle();
 copyrightYear();
+getFormData();
