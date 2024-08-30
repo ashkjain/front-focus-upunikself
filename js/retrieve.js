f48270035605bd207ee9677e259c38335f3ded0f
Ashish Jain <ashishjain99@yahoo.com>
2024-08-29 18:21:38 -0600

    Now Under Testing

--- js/retrieve.js
+++ js/retrieve.js
@@ -1,5 +1,4 @@
-function retrieveFormData()
-{
+function retrieveFormData() {
     const firstName = localStorage.getItem('fname');
     const lastName = localStorage.getItem('lname');
     const email = localStorage.getItem('email');
@@ -7,14 +6,12 @@ function retrieveFormData()
     const displayName = document.getElementById('fullName');
     const displayEmail = document.getElementById('email');
 
-    if(lastName == "")
-    {
-        displayName.innerText = `${firstName[0]+firstName.slice(1)}`;
+    if (lastName == "") {
+        displayName.innerText = `${firstName[0] + firstName.slice(1)}`;
         displayEmail.innerText = email;
     }
-    else
-    {
-        displayName.innerText = `${firstName[0].toUpperCase()+firstName.slice(1)} ${lastName[0].toUpperCase()+lastName.slice(1)}`;
+    else {
+        displayName.innerText = `${firstName[0].toUpperCase() + firstName.slice(1)} ${lastName[0].toUpperCase() + lastName.slice(1)}`;
         displayEmail.innerText = email;
     }
 };
--- js/script.js
+++ js/script.js
@@ -2,37 +2,33 @@ function hamburgerToggle() {
     const hamburger = document.getElementById('hamburgerButton');
     const navigation = document.querySelectorAll('.navLinks');
     hamburger.addEventListener('click', () => {
-        navigation.forEach((nav) => 
-        {
+        navigation.forEach((nav) => {
             nav.classList.toggle('hiddenNav');
         })
     });
 };
 
-function copyrightYear()
-{
+function copyrightYear() {
     const currentYear = new Date().getFullYear();
     const yearElement = document.getElementById('year');
-    yearElement.innerText = currentYear;   
+    yearElement.innerText = currentYear;
 };
 
-function getFormData()
-{
+function getFormData() {
     const contactForm = document.getElementById('form');
-    contactForm.addEventListener('submit', (event) => 
-        {
-            event.preventDefault();
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-            const firstName = document.getElementById('fname').value;
-            const lastName = document.getElementById('lname').value;
-            const email = document.getElementById('email').value;
+        const firstName = document.getElementById('fname').value;
+        const lastName = document.getElementById('lname').value;
+        const email = document.getElementById('email').value;
 
-            localStorage.setItem('fname', firstName);
-            localStorage.setItem('lname', lastName);
-            localStorage.setItem('email', email);
+        localStorage.setItem('fname', firstName);
+        localStorage.setItem('lname', lastName);
+        localStorage.setItem('email', email);
 
-            window.location.href = 'thanks.html';
-        });
+        window.location.href = 'thanks.html';
+    });
 };
 
 copyrightYear();
